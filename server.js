07ff672b17ad9f23ff49e3feab6fa936456652cf
diadem <igor@berlenko.com>
2021-10-18 00:17:20 +0800

    update

--- server.js
+++ server.js
@@ -1,18 +1,71 @@
 import server from 'server'
+import path from 'path'
+import { rename } from 'fs/promises'
+import cors from 'cors'
 
 const { get, post } = server.router
 
+const { header } = server.reply
+
 const port = 3001
 
-server({ port }, [
-    post('/create', ctx => {
-        return {status: 200, body: 'OK'}
+const corsMiddleware = server.utils.modern(
+    cors({
+        origin: ['http://localhost:8080']
+    })
+)
+
+server({ port }, corsMiddleware, [
+    post('/record', ctx => {
+        console.dir(ctx.params)
+
+        console.dir(ctx.query)
     }),
+
+    get('/record', ctx => {
+        console.dir(ctx.params)
+
+        console.dir(ctx.query)
+    }),
+
     get('/', ctx => 'Hello from API'),
+
     get('/tapes/:creator', ctx => ({
         creator: ctx.params.creator,
         tapes: []
-    }))
-])
+    })),
 
-console.log(`server is running on port ${port} ... ${new Date().toUTCString()}`)
+    // maybe file initially should be saved to moralis too
+
+    // how do we check user is owner of given token id
+
+    // probably should call moralis cloud function
+
+    // and that function will execute post action via webhook
+
+    // because moralis performs authentication
+
+    post('/create', async ctx => {
+        console.log('here...')
+
+        console.dir(ctx.params)
+
+        const tokenId = ctx.params.tokenId
+
+        const audio = ctx.files.audio
+
+        const tempPath = audio.path
+
+        if (path.extname(audio.originalname).toLowerCase() !== '.webm') {
+            throw 'invalid file format'
+        }
+
+        const newPath = path.join(__dirname, `audio/${tokenId}.webm`)
+
+        await rename(tempPath, newPath)
+        
+        return {status: 200, body: 'OK'}
+    })
+]).then(ctx => {
+    console.log(`server is running on port ${ctx.options.port} ... ${new Date().toUTCString()}`)    
+})
