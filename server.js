82db52281139ac70e83057272239fc6cbdc59005
diadem <igor@berlenko.com>
2021-10-18 12:08:17 +0800

    update

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 import server from 'server'
+import fs from 'fs'
+import axios from 'axios'
 
 const { get, post } = server.router
+
+async function saveAudio({ tokenId, ethAddress, url }) {
+    const audioPath = path.join(__dirname, `audio/${tokenId}.webm`)
+    
+    const writer = fs.createWriteStream(audioPath)
+    
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    })
+
+    response.data.pipe(writer)
+
+    writer.on('error', function (err) {
+        console.log(`failed downloading file... ${err.toString()} ... ${new Date()}`)
+    })
+
+    writer.on('finish', function () {
+        console.log(`successfully saved file... ${audioPath}`)
+    })
+}
 
 server({
     port: 3001,
     security: false
 }, [
-    post('/record', (ctx) => {
-        const { audio } = ctx.body.object
+    post('/record', async (ctx) => {
+        const { audio, tokenId } = ctx.body.object
         
         const { ethAddress } = ctx.body.user
 
-        console.dir(audio, { depth: null })
+        const { url } = audio
+
+        saveAudio({
+            tokenId,
+            ethAddress,
+            url
+        })
         
-        console.log({ ethAddress })
-    
         return 'OK'
     })
 ])
