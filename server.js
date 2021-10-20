f9fffd35115fe8a179923a1010d8ca07bc6f8688
diadem <igor@berlenko.com>
2021-10-20 18:40:19 +0800

    update

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const { get, post } = server.router
 
 async function saveAudio({ tokenId, ethAddress, url }) {
     const audioPath = `${path.resolve()}/audio/${tokenId}.webm`
-    
+
     const writer = fs.createWriteStream(audioPath)
-    
+
     const response = await axios({
         url,
         method: 'GET',
@@ -28,12 +28,53 @@ async function saveAudio({ tokenId, ethAddress, url }) {
     })
 }
 
+const emptyTapeMetadata = {
+    "name": "Empty Tape",
+    "image": "https://gateway.pinata.cloud/ipfs/QmVgHb2gUgKbmNFmfLBor1iyNdSWFW1UVJmvideSWUAjXN",
+    "external_url": "https://cryptotaperecordings.com",
+    "description": "Available for masterpiece recording",
+    "attributes": [
+        {
+            "trait_type": "rarity",
+            "value": "gold"
+        }
+    ]
+}
+
+const sampleTapeMetadata = {
+    "name": "Sample Song",
+    "image": "https://gateway.pinata.cloud/ipfs/QmW9GUSf2u8S6q96KAr1Umq1J3c7vpXHJQVJhP6kma5Fr4",
+    "external_url": "https://api.cryptotaperecordings.com/token/1",
+    "description": `
+    J'ai cherché Agapé, j'ai cherché Agapé, j'ai cherché Agapé 
+J'ai cherché Agapé, j'ai cherché Agapé, j'ai cherché Agapé 
+J'ai cherché Agapé, j'ai cherché Agapé, j'ai cherché Agapé 
+J'ai cherché Agapé, j'ai cherché Agapé, j'ai cherché Agapé 
+    `,
+    "attributes": [
+        {
+            "trait_type": "rarity",
+            "value": "gold"
+        },
+        {
+            "trait_type": "audio",
+            "value": "https://gateway.pinata.cloud/ipfs/QmV55qseMjrj8HdFhx2qmJ6XSpUXwisjafJkGJyfhDG5Rw"
+        }
+    ]
+}
+
 server({
     port: 3001,
     security: false
 }, [
     get('/token/:tokenId', async (ctx) => {
         const { tokenId } = ctx.params
+
+        if (tokenId == 1) {
+            return sampleTapeMetadata
+        } else {
+            return emptyTapeMetadata
+        }
 
         const t = new TapeObject()
 
@@ -74,17 +115,17 @@ server({
     post('/afterSaveTape', async (ctx) => {
         try {
             const { audio, tokenId } = ctx.body.object
-        
+
             const { ethAddress } = ctx.body.user
-    
+
             const { url } = audio
-    
+
             saveAudio({
                 tokenId,
                 ethAddress,
                 url
-            })    
-        } catch (e) {}
+            })
+        } catch (e) { }
 
         return 'OK'
     })
